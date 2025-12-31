5067e5afb3461e355bccc3782bfd5bd9e64db5dd
Kailas R Pillai <101397280+KAILAS-R-PILLAI@users.noreply.github.com>
2026-01-01 02:08:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`React app running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`React app running on port ${PORT}`));
